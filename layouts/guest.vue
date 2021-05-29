f79d2ebe4f64505ea6711d1deae5b2de254f99c8
Joseledgy <jrl22ledger@gmail.com>
2021-05-29 19:00:54 +0300

    Update the look for the default layout, this will reflect in all of the pages when a user is logged in. Change the layout for the home page, displaying welcome to sales. Added a guest page for users that are not logged in. Change theme to light mode.

---
+++ layouts/guest.vue
@@ -0,0 +1,43 @@
+<template>
+	<v-app dark>
+
+		<v-app-bar
+			fixed
+			flat
+			app
+			dense
+		>
+			<v-toolbar-title v-text="title" />
+			<v-spacer />
+		</v-app-bar>
+
+		<v-content>
+			<v-container>
+			<nuxt />
+			</v-container>
+		</v-content>
+		
+		<v-footer
+			fixed
+			app
+		>
+			<span>&copy; {{ new Date().getFullYear() }}</span>
+		</v-footer>
+	</v-app>
+</template>
+
+<script>
+
+export default {
+	data () {
+		return {
+			title: 'Sales'
+		}
+	},
+	methods: {
+	}
+}
+</script>
+
+<style>
+</style>
